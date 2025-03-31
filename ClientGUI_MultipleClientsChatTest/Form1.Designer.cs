668cf9c64b502eef6ef0f04ef2b274c0aa184f2a
neonsn0w <pietro@eprase.com>
2025-03-31 08:58:40 +0200

    Add project files.

---
+++ ClientGUI_MultipleClientsChatTest/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ClientGUI_MultipleClientsChatTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(18, 18);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(913, 470);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 506);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(788, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(820, 506);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 555);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ClientGUI_MultipleClientsChatTest/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace ClientGUI_MultipleClientsChatTest
+{
+    public partial class Form1 : Form
+    {
+        private Socket senderSocket;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readMessages(Socket reader)
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            try
+            {
+                while (true)
+                {
+                    bytesRead = reader.Receive(buffer);
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine(message);
+                    this.richTextBox1.Invoke(new Action(() => this.richTextBox1.AppendText(message + Environment.NewLine)));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+        private void addTrailingTextToTextBox(string text)
+        {
+            richTextBox1.Text = richTextBox1.Text + text + "\n";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+                senderSocket = new Socket(ipAddr.AddressFamily,
+                        SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    senderSocket.Connect(localEndPoint);
+
+                    Thread clientThread = new Thread(() => readMessages(senderSocket));
+                    clientThread.Start();
+
+                    addTrailingTextToTextBox("Socket connected to -> " + senderSocket.RemoteEndPoint.ToString());
+
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string userMessage = textBox1.Text;  // Get the text from TextBox1
+
+            if (!string.IsNullOrEmpty(userMessage))
+            {
+                byte[] messageSent = Encoding.UTF8.GetBytes(userMessage + "<EOF>");
+                int byteSent = senderSocket.Send(messageSent);
+                textBox1.Clear();  // Clear the TextBox after sending the message
+
+                // Optionally, display the sent message in the richTextBox
+                addTrailingTextToTextBox("You: " + userMessage);
+
+                // If user types "exit", close the application
+                if (userMessage.ToLower() == "exit")
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a message.");
+            }
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userMessage = textBox1.Text;  // Get the text from TextBox1
+
+            if (!string.IsNullOrEmpty(userMessage))
+            {
+                byte[] messageSent = Encoding.UTF8.GetBytes(userMessage + "<EOF>");
+                int byteSent = senderSocket.Send(messageSent);
+                textBox1.Clear();  // Clear the TextBox after sending the message
+
+                // Optionally, display the sent message in the richTextBox
+                addTrailingTextToTextBox("You: " + userMessage);
+
+                // If user types "exit", close the application
+                if (userMessage.ToLower() == "exit")
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a message.");
+            }
+        }
+    }
+}
