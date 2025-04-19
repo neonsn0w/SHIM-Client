e3ac09208c015e43ba3ca299661e5c20d132939c
neonsn0w <pietro@eprase.com>
2025-04-19 17:36:42 +0200

    uuughhh

---
+++ ClientGUI_MultipleClientsChatTest/MainList.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ClientGUI_MultipleClientsChatTest
+{
+    public partial class MainList : Form
+    {
+        public MainList()
+        {
+            InitializeComponent();
+        }
+    }
+}
