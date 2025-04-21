40454435b521cfcaf1a3efbe586b78b9fbd502e0
neonsn0w <pietro@eprase.com>
2025-04-21 22:32:33 +0200

    cooking

---
+++ ClientGUI_MultipleClientsChatTest/UsernameInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ClientGUI_MultipleClientsChatTest
+{
+    public partial class UsernameInput : Form
+    {
+        public UsernameInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
