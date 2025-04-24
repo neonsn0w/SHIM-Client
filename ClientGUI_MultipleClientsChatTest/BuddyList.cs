c0f1d0ee248fd79dcaadf0ae0991328d27be62ed
neonsn0w <pietro@eprase.com>
2025-04-24 21:30:30 +0200

    Added buddyList

---
+++ ClientGUI_MultipleClientsChatTest/BuddyList.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ClientGUI_MultipleClientsChatTest
+{
+    public partial class BuddyList : Form
+    {
+        public BuddyList()
+        {
+            InitializeComponent();
+        }
+    }
+}
